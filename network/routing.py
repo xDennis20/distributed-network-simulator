dd05c45a855c71e86b3757e07bf9c0dbf190975f
xDennis20 <dennis_6@outlook.com>
2026-03-13 16:42:00 -0500

    feat: Nuevo metodo construir_red

--- network/routing.py
+++ network/routing.py
@@ -7,3 +7,9 @@ class Grafo:
     def agregar_nodo(self,valor: str) -> None:
         nodo = Node(valor)
         self.nodos[valor] = nodo
+
+    def construir_red(self, nodos: dict[str,list[str]]) -> None:
+        for nombre,vecino in nodos.items():
+            self.agregar_nodo(nombre)
+            nodo = self.nodos.get(nombre)
+            nodo.vecinos = vecino
