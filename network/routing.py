a465a31212a58673f9004c77e94d3b5667b5208b
xDennis20 <dennis_6@outlook.com>
2026-03-18 12:12:19 -0500

    feat: Separacion de Responsabilidades

--- network/routing.py
+++ network/routing.py
@@ -1,25 +1,5 @@
-from network.node import Node
 from strategy.bfs import recorrer_bfs
 
-class Grafo:
-    def __init__(self):
-        self.nodos: dict[str,Node] = {}
-
-    def agregar_nodo(self,valor: str) -> None:
-        nodo = Node(valor)
-        self.nodos[valor] = nodo
-
-    def construir_red(self, nodos: dict[str,list[str]]) -> None:
-        for nombre,vecino in nodos.items():
-            self.agregar_nodo(nombre)
-            nodo = self.nodos.get(nombre)
-            nodo.vecinos = vecino
-
-    def inicializar_enrutadores(self):
-        for nodo in self.nodos.values():
-            recorrer_bfs(nodo,self.nodos)
-            print(nodo.tabla_enrutamiento)
-
-
-    def __str__(self):
-        return f"Grafo: {self.nodos}"
+def inicializar_enrutadores(nodos: dict):
+    for nodo in nodos.values():
+        recorrer_bfs(nodo,nodos)
---
+++ network/topology.py
@@ -0,0 +1,18 @@
+from network.node import Node
+
+class Grafo:
+    def __init__(self):
+        self.nodos: dict[str,Node] = {}
+
+    def agregar_nodo(self,valor: str) -> None:
+        nodo = Node(valor)
+        self.nodos[valor] = nodo
+
+    def construir_red(self, nodos: dict[str,list[str]]) -> None:
+        for nombre,vecino in nodos.items():
+            self.agregar_nodo(nombre)
+            nodo = self.nodos.get(nombre)
+            nodo.vecinos = vecino
+
+    def __str__(self):
+        return f"Grafo: {self.nodos}"
