f1ebd4f7c0a1f1805cfdb08e19062469888fbf6d
xDennis20 <dennis_6@outlook.com>
2026-03-20 10:24:52 -0500

    feat: ID autoincrementado en paquete

--- packets/models.py
+++ packets/models.py
@@ -1,12 +1,14 @@
 from dataclasses import dataclass, field
+from itertools import count
 from datetime import datetime
 
+generador_id = count(1)
 @dataclass(frozen=True)
 class Paquete:
-    id: int
-    fuente: str
+    origen: str
     destino: str
     contenido: str
     prioridad: int
     tiempo_evento: datetime
     nodos_recorridos: list[str] = field(default_factory=list)
+    id: int = field(default_factory=generador_id.__next__)
